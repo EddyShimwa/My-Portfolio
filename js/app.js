3857fa1e15e440a1708528e8e896a81ec6092450
EddyShimwa <shedpray16@gmail.com>
2022-11-18 22:16:56 +0200

    Driver one added the form validation

--- js/app.js
+++ js/app.js
@@ -160,3 +160,32 @@ actionsBtn.forEach((btn) => {
     fetchOneProject(id);
   });
 });
+
+//Validation of the form
+function onSubmit(e) {
+  const inputEmail = document.getElementById('email');
+  const formInfo = document.getElementById('form-info');
+  const email = inputEmail.value;
+
+  // Check if email value is lowercase or not
+  if (email !== email.toLowerCase()) {
+    e.preventDefault();
+    inputEmail.classList.add('invalid');
+    formInfo.classList.add('error');
+    formInfo.innerText = 'Error submitting form! The Email should be in lower case!!';
+  } else {
+    inputEmail.classList.remove('invalid');
+    formInfo.classList.remove('error');
+  }
+}
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', onSubmit);
+
+// Remove Error onchange from the form.
+const inputEmail = document.getElementById('email');
+const formInfo = document.getElementById('form-info');
+inputEmail.addEventListener('change', () => {
+  inputEmail.classList.remove('invalid');
+  formInfo.classList.remove('error');
+  formInfo.innerText = '';
+});
